b784a68dec3ea74e7ee7c55ad1001c61ef71f61b
aguayamante-ui <agu.ayamante@gmail.com>
2026-06-22 17:02:13 -0400

    Add countdown timer and coupon display functionality
    Implement countdown timer and coupon display function

---
+++ script.js
@@ -0,0 +1,45 @@
+let fecha = new Date("June 27, 2026 18:00:00").getTime();
+
+
+setInterval(function(){
+
+
+let ahora = new Date().getTime();
+
+
+let distancia = fecha - ahora;
+
+
+
+let dias = Math.floor(distancia/(1000*60*60*24));
+
+let horas = Math.floor((distancia%(1000*60*60*24))/(1000*60*60));
+
+let minutos = Math.floor((distancia%(1000*60*60))/(1000*60));
+
+let segundos = Math.floor((distancia%(1000*60))/1000);
+
+
+
+document.getElementById("contador").innerHTML =
+
+dias+" días "+horas+" horas "+minutos+" minutos "+segundos+" segundos";
+
+
+},1000);
+
+
+
+
+function mostrarCupon(){
+
+
+document.getElementById("premio").innerHTML=
+
+"❤️ Cupón válido para Dani ❤️<br><br>"+
+"🎟️ Actividad 1: Una cita especial juntos<br>"+
+"🎟️ Actividad 2: Una actividad elegida por ti<br>"+
+"🎟️ Actividad 3: Un día completo dedicado a nosotros";
+
+
+}
